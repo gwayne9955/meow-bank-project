a78e36428a5c5c1ab756423ed9191662733e5f0a
Garrett Wayne <garrett@postal.io>
2024-12-14 20:45:36 -0800

    feat: backend supports cors from 3000

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
-
+from starlette.middleware.cors import CORSMiddleware
 from src.routers import users, accounts, transfers
 
 app = FastAPI()
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # React app's address
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 
 # Include routers
