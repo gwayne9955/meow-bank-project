ead07d56cc042efd8114de366e5f01282171ad48
Garrett Wayne <garrett@postal.io>
2024-12-14 20:45:50 -0800

    feat: backend sorts accounts by id by default

--- backend/src/repositories/base.py
+++ backend/src/repositories/base.py
@@ -1,5 +1,6 @@
 from sqlalchemy.orm import Session
 from typing import Generic, TypeVar, Type
+from sqlalchemy import desc, asc
 
 T = TypeVar("T")
 
@@ -11,5 +12,16 @@ class BaseRepository(Generic[T]):
     def get(self, id: int) -> T:
         return self.db.query(self.model).filter(self.model.id == id).first()
 
-    def list(self):
-        return self.db.query(self.model).all()
+    def list(self, sort_by: str = 'id', descending: bool = False):
+        query = self.db.query(self.model)
+
+        # Get the column to sort by
+        sort_column = getattr(self.model, sort_by, self.model.id)
+
+        # Apply sort direction
+        if descending:
+            query = query.order_by(desc(sort_column))
+        else:
+            query = query.order_by(asc(sort_column))
+
+        return query.all()
